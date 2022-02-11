bc19e08e384eea6f45b4d7c5d4e4259a6af0d32c
marcodiazz <75041809+marcodiazz@users.noreply.github.com>
2022-02-11 17:27:45 +0100

    Added DarkMode
    Redisgn and darkmode

--- Script.js
+++ Script.js
@@ -1,3 +1,10 @@
+var maze;
+
+genGrid()
+genEmptyMaze();
+genWalls();
+render();
+
 function genGrid()
 {
     var lab = document.getElementById("lab-div");
@@ -8,7 +15,55 @@ function genGrid()
             box.className = "box";
             box.id = "box-" + i + j;
             lab.appendChild(box);
+            console.log("hola")
         }
     }
 }
+
+
+function genEmptyMaze(){
+    maze = [];
+    for(var i=0; i < 10; i++){
+        maze.push([]);
+        for(var j=0; j<10; j++){
+            maze[i][j] = 1;
+        }
+    }
+}
+function genWalls(){
+    for(var i=0; i < 10; i++){
+        for(var j=0; j<10; j++){
+            if (Math.random() > 0.6) maze[i][j] = 1;
+            else maze[i][j] = 0;
+        }
+    }
+}
+
+function render(){
+    for(var i=0; i < 10; i++){
+        for(var j=0; j<10; j++){
+            if(maze[i][j] == 1){
+                getBox(i,j).classList.add("wall");
+            }
+            else if(maze[i][j] == 0){
+                getBox(i,j).classList.add("path");
+            }
+           
+        }
+    } 
+}
+
+function move(){
     
+}
+
+function getBox(i, j){
+    return document.getElementById("box-" + i + j);
+}
+
+function darkMode(){  
+    document.body.classList.toggle("dark-mode-body");
+}
+function hide(){
+    document.getElementById("mascot1").classList.toggle("hide-mascot");
+}
