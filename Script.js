b64d3971ae716087c5df9d82e3c6cf778fce2cd2
marcodiazz <75041809+marcodiazz@users.noreply.github.com>
2022-03-08 12:08:34 +0100

    Responsive web improvement and first slide version

--- Script.js
+++ Script.js
@@ -180,3 +180,54 @@ function darkMode(){
     document.body.classList.toggle("dark-mode-body");
 }
 
+//----------MOVIMIENTO------------
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            console.log("right");
+            /* right swipe */ 
+        } else {
+            console.log("left");
+
+            /* left swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            console.log("down");
+            /* down swipe */ 
+        } else { 
+            console.log("up");
+            /* up swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
